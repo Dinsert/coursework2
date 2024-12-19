5602ed0a79b767129b0efd908675cffa1eafb8d7
Наталья <korolkov.01@bk.ru>
2024-12-19 21:31:57 +0300

    coursework2

--- src/test/java/org/skypro/question/service/ExaminerServiceImplTest.java
+++ src/test/java/org/skypro/question/service/ExaminerServiceImplTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.skypro.question.domain.Question;
@@ -20,19 +19,24 @@ import org.skypro.question.repository.MathQuestionRepository;
 @ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
 
+    @Mock
+    private JavaQuestionRepository javaQuestionRepositoryMock;
+    @Mock
+    private MathQuestionRepository mathQuestionRepositoryMock;
     private JavaQuestionService javaQuestionService;
     private MathQuestionService mathQuestionService;
-    private final JavaQuestionRepository javaQuestionRepository = mock(JavaQuestionRepository.class);
-    private final MathQuestionRepository mathQuestionRepository = mock(MathQuestionRepository.class);
     private ExaminerServiceImpl out;
-
-
-    private Collection<Question> questions;
+    private Collection<Question> questions = new HashSet<>(List.of(
+            new Question("question1", "answer1"),
+            new Question("question2", "answer2"),
+            new Question("question3", "answer3"),
+            new Question("question4", "answer4"),
+            new Question("question5", "answer5")));
 
     @BeforeEach
     void setUp() {
-        javaQuestionService = new JavaQuestionService(javaQuestionRepository);
-        mathQuestionService = new MathQuestionService(mathQuestionRepository);
+        javaQuestionService = new JavaQuestionService(javaQuestionRepositoryMock);
+        mathQuestionService = new MathQuestionService(mathQuestionRepositoryMock);
         out = new ExaminerServiceImpl(javaQuestionService, mathQuestionService);
         questions = new HashSet<>(List.of(
                 new Question("question1", "answer1"),
@@ -44,10 +48,14 @@ class ExaminerServiceImplTest {
 
     @Test
     void getQuestions() {
-        when(javaQuestionRepository.getAll()).thenReturn(questions);
-        when(javaQuestionRepository.getRandomQuestion()).thenReturn(new Question("question1", "answer1"),
-                                                                 new Question("question2", "answer2"),
-                                                                 new Question("question3", "answer3"));
+        when(javaQuestionRepositoryMock.getAll()).thenReturn(questions);
+        when(javaQuestionRepositoryMock.getRandomQuestion()).thenReturn(new Question("question1", "answer1"),
+                                                                        new Question("question2", "answer2"),
+                                                                        new Question("question3", "answer3"));
+        when(mathQuestionRepositoryMock.getAll()).thenReturn(questions);
+        when(mathQuestionRepositoryMock.getRandomQuestion()).thenReturn(new Question("question1", "answer1"),
+                                                                        new Question("question2", "answer2"),
+                                                                        new Question("question3", "answer3"));
         Collection<Question> actual = out.getQuestions(3, null);
         Collection<Question> expected = new HashSet<>(List.of(
                 new Question("question1", "answer1"),
@@ -58,7 +66,7 @@ class ExaminerServiceImplTest {
 
     @Test
     void shouldThrowQuestionsOverflowExceptionAtGetQuestions() {
-        when(javaQuestionRepository.getAll()).thenReturn(questions);
+        when(javaQuestionRepositoryMock.getAll()).thenReturn(questions);
         assertThrows(QuestionsOverflowException.class, () -> out.getQuestions(6, null));
     }
 }
